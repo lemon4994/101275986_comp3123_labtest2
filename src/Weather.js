c7b0a56932fecbf977518be47fba1e5bdb7917a2
Nicholas Chiarello <madeupmon@gmail.com>
2024-11-28 17:05:26 -0500

    added images

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,10 @@ function WeatherDisplay({ weather }) {
   return (
     <div>
       <h1>{weather.name}</h1>
-      <h2>Weather Conditions: {weather.weather[0].description}</h2>
+      <div>
+        <h2>Weather Conditions: {weather.weather[0].description.replace(/\b\w/g, char => char.toUpperCase())}</h2>
+        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="Weather Condition Icon" />
+    </div>
       <h3>Temperature: {Math.round(weather.main.temp - 273.15)} C</h3>
     </div>
   );
